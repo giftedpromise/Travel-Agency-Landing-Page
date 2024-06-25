3ea5c6d315ab44c48bd0c375030be0abcf8e8178
giftedpromise <giftedpromise1992@gmail.com>
2024-06-24 15:12:08 -1200

    set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
 
-const App = () => {
-  return <div>App</div>;
-};
-
+function App() {
+  return (
+    <div className="bg-blue-500 p-4 text-white">
+      <h1 className="text-2xl">Hello, Tailwind!</h1>
+    </div>
+  );
+}
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      colors: {
+        primary: "#DF6951",
+        secondary: "#F1A501",
+        lightBlue: "#181E4B",
+        lightGray: "#5E6282",
+        lightGrayAlt: "#686D77",
+        lightGrayAlt2: "#84829A",
+        white: "#fff",
+        title: "#14183E",
+        subtitle: "#1E1D4C",
+        lightBlack: "#080809",
+        lightPink: "#8A79DF",
+        navText: "#212832",
+      },
+    },
   },
   plugins: [],
 };
