7c19ed009857991ef23fbecd90931bf9383eedb3
giftedpromise <giftedpromise1992@gmail.com>
2024-07-10 22:10:29 +0100

    orderconfirmation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,114 +1,106 @@
 import React, { useState } from "react";
 import { X } from "lucide-react";
-import { Link } from "react-router-dom"; // Importing Link for navigation
-import MainButton from "./MainButton"; // Importing a custom button component
+import { Link } from "react-router-dom";
+import MainButton from "./MainButton";
 
 function NavBar() {
-  const [menu, setMenu] = useState(false); // State to manage the menu's open/close status
+  const [menu, setMenu] = useState(false);
   const toggleMenu = () => {
-    setMenu(!menu); // Function to toggle the menu state
+    setMenu(!menu);
   };
 
   return (
     <div className="z-20 md:top-0 md:shadow-none">
       {/* DESKTOP VIEW */}
-      <div
-        className="animate-in fade-in zoom-in hidden p-4 lg:block"
-        style={{ overflowX: "hidden" }}
-      >
+      <div className="hidden p-4 lg:block">
         <div className="flex items-center justify-between">
           <div>
-            <img src="/images/logo.png" alt="logo" /> {/* Logo */}
+            <img src="/images/logo.png" alt="logo" className="h-12 w-auto" />
           </div>
-          <div className="flex select-none items-center gap-[20px] text-[16px] xl:gap-[50px]">
-            {/* Navigation links */}
-            <p className="flex cursor-pointer items-center gap-2 font-google-sans text-17px font-normal text-navText hover:text-primary">
+          <div className="flex items-center gap-6 text-lg">
+            <p className="cursor-pointer text-navText hover:text-primary">
               Destinations
             </p>
-
-            <p className="flex cursor-pointer items-center gap-2 font-google-sans text-17px font-normal text-navText hover:text-primary">
+            <p className="cursor-pointer text-navText hover:text-primary">
               Hotels
             </p>
-            <p className="flex cursor-pointer items-center gap-2 font-google-sans text-17px font-medium text-navText hover:text-primary">
+            <p className="cursor-pointer text-navText hover:text-primary">
               Flights
             </p>
-            <p className="flex cursor-pointer items-center gap-2 font-google-sans text-17px font-medium text-navText hover:text-primary">
+            <p className="cursor-pointer text-navText hover:text-primary">
               Bookings
             </p>
             <Link
               to="/auth/login"
-              className="flex cursor-pointer items-center gap-2 font-google-sans text-17px font-medium text-navText hover:text-primary"
+              className="cursor-pointer text-navText hover:text-primary"
             >
               Login
             </Link>
             <MainButton
               text="Sign up"
-              classes="text-black bg-transparent font-google-sans text-17px shadow-none rounded-normal border border-navText hover:border-none hover:text-white"
+              classes="text-black bg-transparent shadow-none border border-navText hover:border-none hover:text-white"
             />
-            <div className="flex cursor-pointer items-center gap-2">
-              <p className="font-google-sans text-17px font-medium">EN</p>
-              <div>
-                <img src="/images/chevron-down.png" alt="chevron down" />
-              </div>
+            <div className="flex cursor-pointer items-center">
+              <p className="text-navText">EN</p>
+              <img
+                src="/images/chevron-down.png"
+                alt="chevron down"
+                className="ml-2 h-4 w-4"
+              />
             </div>
           </div>
         </div>
       </div>
-      {/* MOBILE VIEW */}
+      {/* MOBILE AND TABLET VIEW */}
       <div
-        className={`animate-in fade-in zoom-in fixed top-0 z-[999] block w-full py-4 shadow-sm lg:hidden ${menu ? "bg-primary py-2" : "bg-white"}`}
-        style={{ overflowX: "hidden" }}
+        className={`fixed top-0 z-50 w-full py-4 shadow-sm lg:hidden ${menu ? "bg-primary py-2" : "bg-white"}`}
       >
-        <div className="mx-[10px] flex justify-between">
-          <div className="flex select-none items-center gap-[50px] text-[16px]">
-            <img src="/images/logo.png" alt="logo" className="w-[7rem]" />
+        <div className="mx-4 flex justify-between">
+          <div className="flex items-center">
+            <img src="/images/logo.png" alt="logo" className="w-28" />
           </div>
-          <div className="flex items-center gap-[40px]">
+          <div className="flex items-center">
             {menu ? (
-              <X
-                className="animate-in fade-in zoom-in cursor-pointer text-black"
-                onClick={toggleMenu}
-              />
+              <X className="cursor-pointer text-black" onClick={toggleMenu} />
             ) : (
               <img
                 src="/images/hamburger.svg"
                 alt="menu"
-                className="animate-in fade-in zoom-in cursor-pointer"
+                className="cursor-pointer"
                 onClick={toggleMenu}
               />
             )}
           </div>
         </div>
         {menu && (
-          <div className="animate-in slide-in-from-right my-8 select-none">
-            <div className="mx-4 mt-8 flex flex-col gap-8">
-              <div className="flex select-none flex-col gap-[20px] text-[16px] xl:gap-[50px]">
-                <p className="flex cursor-pointer items-center gap-2 font-[600] text-navText hover:text-white">
-                  Destinations
-                </p>
-
-                <p className="flex cursor-pointer items-center gap-2 font-[600] text-navText hover:text-white">
-                  Hotels
-                </p>
-                <p className="flex cursor-pointer items-center gap-2 font-[600] text-navText hover:text-white">
-                  Flights
-                </p>
-                <p className="flex cursor-pointer items-center gap-2 font-[600] text-navText hover:text-white">
-                  Bookings
-                </p>
-                <Link
-                  to="/auth/login"
-                  className="flex cursor-pointer items-center gap-2 font-[600] text-navText hover:text-white"
-                >
-                  Login
-                </Link>
-                <MainButton text="Sign up" />
-                <div className="flex cursor-pointer items-center gap-2">
-                  <p className="font-[700]">EN</p>
-                  <div>
-                    <img src="/images/chevron-down.png" alt="chevron down" />
-                  </div>
-                </div>
+          <div className="mt-8 flex flex-col gap-8">
+            <div className="mx-4 flex flex-col gap-6 text-lg">
+              <p className="cursor-pointer text-navText hover:text-white">
+                Destinations
+              </p>
+              <p className="cursor-pointer text-navText hover:text-white">
+                Hotels
+              </p>
+              <p className="cursor-pointer text-navText hover:text-white">
+                Flights
+              </p>
+              <p className="cursor-pointer text-navText hover:text-white">
+                Bookings
+              </p>
+              <Link
+                to="/auth/login"
+                className="cursor-pointer text-navText hover:text-white"
+              >
+                Login
+              </Link>
+              <MainButton text="Sign up" />
+              <div className="flex cursor-pointer items-center">
+                <p className="font-bold text-navText">EN</p>
+                <img
+                  src="/images/chevron-down.png"
+                  alt="chevron down"
+                  className="ml-2 h-4 w-4"
+                />
               </div>
             </div>
           </div>
